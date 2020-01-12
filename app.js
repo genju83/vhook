cac4d760cd20335adc09dce9f32298ff93fe779f
Victor <victor.park@kakaocorp.com>
2020-01-13 00:41:00 +0900

    add pages route

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var pages = require('./routes/pages');
 var hooks = require('./routes/hooks');
 
 var app = express();
@@ -9,6 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
 
+app.use('/', pages)
 app.use('/api/v1/hooks', hooks);
 
 module.exports = app;
--- routes/hooks.js
+++ routes/hooks.js
@@ -2,10 +2,6 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios').default;
 
-router.get('/', function (req, res, next) {
-    res.json({ result: "OK" });
-});
-
 router.post('/', function (req, res, next) {
     let url = req.body.url;
     let config = { headers: req.body.headers };
